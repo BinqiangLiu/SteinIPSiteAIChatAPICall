f050679b3367223245becac26f63adc19a710a9f
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-12-05 16:38:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 load_dotenv()
 import json  # 导入json模块
 
-st.set_page_config(page_title="USinoIP Website AI Chat Assistant - Open Source Version", layout="wide")
-st.subheader("Welcome to USinoIP Website AI Chat Assistant - Open Source Version.")
-st.write("Important notice: This USinoIP Website AI Chat Assistant is offered ONLY for the purpose of assisting users to better interact with USinoIP webiste contents and by no means for any other use. Any user should never interact with the AI Assistant in any way that is against any related promulgated regulations. The user is the only entity responsible for interactions taken between the user and the AI Chat Assistant.")
+st.set_page_config(page_title="SteinIP Website AI Chat Assistant", layout="wide")
+st.subheader("Welcome to SteinIP Website AI Chat Assistant.")
+st.write("Important notice: This SteinIP Website AI Chat Assistant is offered ONLY for the purpose of assisting users to better interact with SteinIP webiste contents and by no means for any other use. Any user should never interact with the AI Assistant in any way that is against any related promulgated regulations. The user is the only entity responsible for interactions taken between the user and the AI Chat Assistant.")
 
 css_file = "main.css"
 with open(css_file) as f:
@@ -15,7 +15,7 @@ with open(css_file) as f:
     
 HUGGINGFACEHUB_API_TOKEN = os.getenv('HUGGINGFACEHUB_API_TOKEN')
 
-def call_chatbot_api(query):
+def aichat(query):
     #url = 'https://binqiangliu-fastapi-in-docker.hf.space/api/chat'
     url = 'https://binqiangliu-steinipsiteaichatapi.hf.space/aisitechat'
     headers = {
@@ -36,7 +36,7 @@ user_query = st.text_input("Enter your query here:")
 if st.button('Get AI Response'):
     with st.spinner("AI Thinking...Please wait a while to Cheers!"):    
         if user_query !="" and not user_query.strip().isspace() and not user_query == "" and not user_query.strip() == "" and not user_query.isspace():
-            response = call_chatbot_api(user_query)
-            st.write("USino AI Response:")
+            response = aichat(user_query)
+            st.write("SteinIP AI Response:")
             st.write(response)
             print(response)  # 打印Chatbot的响应
